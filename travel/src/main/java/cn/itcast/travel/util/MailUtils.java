0408087c6f25af35617fe134a27e01d01c0c8c65
jiangzuchang <18779194245@139.com>
2019-12-26 10:31:49 +0800

    first commit

--- travel/src/main/java/cn/itcast/travel/util/MailUtils.java
+++ travel/src/main/java/cn/itcast/travel/util/MailUtils.java
@@ -9,8 +9,8 @@ import java.util.Properties;
  * 发邮件工具类
  */
 public final class MailUtils {
-    private static final String USER = "five_thirty_am@qq.com"; // 发件人称号，同邮箱地址
-    private static final String PASSWORD = "juzmklxylmsbbgac"; // 如果是qq邮箱可以使客户端授权码，或者登录密码
+    private static final String USER = ""; // 发件人邮箱地址
+    private static final String PASSWORD = ""; // 可以使客户端授权码，或者登录密码
 
     /**
      *
